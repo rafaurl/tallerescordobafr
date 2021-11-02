9aa0ae89650159dbddaa8e5e393bdadb516c8976
rafaurl <rafaurl@gmail.com>
2021-11-02 21:24:07 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+(function ($) {
+    'use strict';
+
+$(window).load(function() {
+    $('#preloader-wrapper').fadeOut('slow');
+});
+
+    $('.gallery-slider').owlCarousel(
+        {
+        pagination : true,
+        autoPlay : 5000,
+        itemsDesktop  :  [1500,4],
+        itemsDesktopSmall :  [979,3]
+        }
+    );
+
+    // Gallery Popup
+    $('.image-popup').magnificPopup({type:'image'});
+
+	//divisor imagenes
+	$(".twentytwenty").twentytwenty({
+		before_label: 'Antes',
+		after_label: 'Después'
+	});
+})(jQuery);
+
+
+
+
+
+
+
